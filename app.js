a8ef2a00a58b9d9289932807b983f221bba3045c
werthzack <timmyabiola27@gmail.com>
2025-11-10 14:46:58 +0000

    allowed cross origin resource sharing

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ const {
   patchArticleById,
   removeComment,
 } = require("./controllers");
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use(express.static("public"));
 app.use(express.json());
 
